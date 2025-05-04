a75d5e7a5696ed2f83b80dcb17a18bceb572c99b
abidm-bit <abidmres@gmail.com>
2025-05-04 18:33:52 -0400

    2nd method to find the square root

--- javaScriptKhyda/sqrRoot.js
+++ javaScriptKhyda/sqrRoot.js
@@ -2,8 +2,20 @@ function returnSqrt(numb){
     console.log(Math.sqrt(numb));
 }
 
+function returnSqrt2(numb){
+    console.log(Math.pow(numb,0.5));
+}
+
 returnSqrt(121); //11 
 
 returnSqrt(100); //10
 
 returnSqrt(144); //12
+
+console.log();
+
+returnSqrt2(121);
+
+returnSqrt2(100);
+
+returnSqrt2(144);
