05ab05114128d8db3c37bc93687d3341d4570aed
abidm-bit <abidmres@gmail.com>
2025-05-04 18:19:14 -0400

    calculate a fibonacci term and sequence

---
+++ javaScriptKhyda/fibo.js
@@ -0,0 +1,25 @@
+const fibonacciTerm = (number)=>{
+if(number<=1){return number};
+return fibonacciTerm(number-1)+fibonacciTerm(number-2);
+}
+
+function fibonacciSequence(number){
+    for(i=1;i<=number;i++){
+        console.log(fibonacciTerm(i));
+    }
+}
+
+console.log(fibonacciTerm(1));
+console.log(fibonacciTerm(2));
+console.log(fibonacciTerm(3));
+
+console.log();
+
+fibonacciSequence(3);
+console.log();
+
+fibonacciSequence(5);
+
+console.log();
+
+fibonacciSequence(8);
