dfb1112dd9fad0a1b3477f91648a5e7e14e3fc13
abidm-bit <abidmres@gmail.com>
2025-05-05 11:16:52 -0400

    define array type

---
+++ typeScriptKhyda/arrayPractice.ts
@@ -0,0 +1,12 @@
+const wweGames: string[]=["WWE XXVI","Smackdown vs Raw 2007","Smackdown vs Raw 2010"];
+
+let supplies: string[]=[]; 
+supplies.push("sticky notes","hydrogen peroxide","coil cleaner","blinds","double sided tape");
+
+for (const x of wweGames){console.log(x);}
+console.log();
+for (let s of supplies){console.log(s);}
+console.log();
+
+let speed: number[]=[86,65,87,64,34,33,75,72];
+for(let spd of speed){console.log(spd);}
